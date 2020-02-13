96acf023f688908985634180d5aac78f354e52c8
Chance Smith <chancesmith97@hotmail.com>
2020-02-13 21:06:34 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,7 +24,6 @@ def won?(board)
    board[location1] == board[location2] && board[location2] == board[location3] && board[location1] != " "
  end
 end
-won?(board)
 
 def full?(board)
   !board.any? {|x| x == " "}
