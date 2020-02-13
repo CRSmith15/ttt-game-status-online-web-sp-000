9e0c9eb33ef02ad2e905d0f537e115e196c4c768
Chance Smith <chancesmith97@hotmail.com>
2020-02-13 20:51:54 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,39 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0,1,2], #top row 
+  [3,4,5], #mid row 
+  [6,7,8], #bot row 
+  [0,3,6], #left col 
+  [1,4,7], #mid col 
+  [2,5,8], #right col
+  [0,4,8], #left diag 
+  [2,4,6] #right diag 
+  ]
+
+def won?(board)
+ winner = []
+empty_board = board.all? {|x| x == " "}
+WIN_COMBINATIONS.each do |win_comb|
+    if empty_board || full?(board)
+      return false
+    elsif win_comb.all? { |value| board[value] =="X" } || win_comb.all? { |value| board[value] =="O" }
+      winner = win_comb
+    end
+  end
+  winner
+end
+
+def full?(board)
+  !board.any? {|x| x == " "}
+end
+
+def draw?(board)
+  if !(won?(board)) && full?(board)
+    return true
+  elsif won?(board)
+    return false
+  end
+end
